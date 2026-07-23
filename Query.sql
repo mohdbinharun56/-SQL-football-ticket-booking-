c14248f202c53a155cab52712527fcc917765d78
mohdbinharun56 <arahaman461@gmail.com>
2026-07-24 02:37:24 +0600

    write Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.

--- Query.sql
+++ Query.sql
@@ -127,3 +127,8 @@ select booking_id, full_name, fixture, total_cost
 select user_id, full_name, booking_id 
   from users 
   left join bookings using(user_id);
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+select booking_id, match_id, total_cost from bookings where total_cost > (select avg(total_cost) from bookings);
